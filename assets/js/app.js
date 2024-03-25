1c7d5fce0e7c444ea90b1c2218115391abd8130f
Davronbek <davroncoder074@gmail.com>
2024-03-25 18:01:32 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// const shaklEl = document.querySelector(".shakl");
+// const bgEl = document.querySelector(".bg_round");
+// const bfEl = document.querySelector(".bf");
+// const formEl = document.querySelector(".form");
+// const btnEl = document.querySelector(".btn3");
+// const contEl = document.querySelector(".container");
+// btnEl.addEventListener("click", () => {
+//     contEl.style.display = "none"
+// })
